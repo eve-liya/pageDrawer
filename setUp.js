dd4e325980a58e077e68748b6c965cb7c61a6d0a
vincent <vincenthawaii4125@gmail.com>
2022-10-15 22:41:05 -1000

    optimization: overflow rendering

--- setUp.js
+++ setUp.js
@@ -1,18 +1,17 @@
+//just a bunch of elements
 const canva = document.createElement("canvas");
 const division = document.createElement("div");
-const bruh = document.createElement("p");
-const bruh2 = document.createTextNode("bruh please");
-//document.documentElement.scrollWidth
+
 division.setAttribute("id", "division");
-canva.setAttribute("width", document.documentElement.scrollWidth, "overflow", "hidden");
+canva.setAttribute("width", document.documentElement.scrollWidth);
+canva.setAttribute("overflow", "hidden")
 canva.setAttribute("height", document.documentElement.scrollHeight);
-bruh.appendChild(bruh2);
 division.appendChild(canva);
-division.appendChild(bruh);
 document.body.insertBefore(division, document.body.firstChild);
 division.setAttribute("height", "100%");
 division.style.zIndex = "1000";
 division.style.position = "absolute";
+
 
 const ctx = canva.getContext('2d');
 let lineWidth = 5;
